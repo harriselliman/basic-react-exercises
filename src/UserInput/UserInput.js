16a2622ee9c7344e0ae70c0a1a0f591551a0371c
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-28 21:32:54 +0100

    add value property

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const userInput = (props) => {
-    return <input type="text" onChange={props.changed}/>
+    return <input
+        type="text"
+        onChange={props.changed}
+        value={props.value} />
 };
 
 export default userInput
