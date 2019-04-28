c879fe8878c45f15f6bed9632979f4431d522915
harriselliman <harris.elliman@hotmail.co.uk>
2019-04-28 20:38:35 +0100

    create userOutput

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>He's our centre half.</p>
+            <p>He's our number 4.</p>
+        </div>
+    );
+};
