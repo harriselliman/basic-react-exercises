fae6408eca9e04922a9a902c060a98d4500d9108
harriselliman <harris.elliman@hotmail.co.uk>
2019-05-02 20:47:51 +0100

    add second paragraph to Validation component

--- lists-conditionals-exercise/src/Validation/Validation.js
+++ lists-conditionals-exercise/src/Validation/Validation.js
@@ -3,6 +3,7 @@ import React from 'react';
 const validation = (props) => {
     return(
         <div>
+            <p>Text long enough</p>
             <p>Text too short</p>
         </div>
     );
