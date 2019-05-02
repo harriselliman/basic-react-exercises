b589b897f1b56a1cf794c6e327f4eef316b5cadd
harriselliman <harris.elliman@hotmail.co.uk>
2019-05-02 20:29:41 +0100

    add hr

--- lists-conditionals-exercise/src/App.js
+++ lists-conditionals-exercise/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+        <hr />
       </div>
     );
   }
